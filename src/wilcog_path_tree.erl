cccac153e31283176e34339a0c311fe3908ef1a5
Akash Manohar J <akash@akash.im>
2014-03-02 23:28:03 +0530

    Fix wilcog_path_tree

--- src/wilcog_path_tree.erl
+++ src/wilcog_path_tree.erl
@@ -1,34 +1,30 @@
 -module(wilcog_path_tree).
--export([build_tree/1, build_tree/2]).
+-export([build/1, build/2]).
 
-build_tree(Path)->
+build(Path)->
   NewTree = gb_trees:empty(),
   PathProperties = [{"path", Path}],
   RootTree = gb_trees:enter(Path, PathProperties, NewTree),
-  build_tree(Path, RootTree).
+  build(Path, RootTree).
 
-build_tree(Path, Tree)->
+
+build(Path, Tree)->
   {ok, Items} = file:list_dir_all(Path),
 
-  ItemMapper = fun(Item)->
-    ParentProps = gb_trees:get(Path, Tree),
+  ItemFolder = fun(Item, AccumulatedTree)->
+    ParentProps = gb_trees:get(Path, AccumulatedTree),
     ParentPath = proplists:get_value("path", ParentProps),
     ItemPath = filename:absname_join(ParentPath, Item),
     LastModifiedAt = filelib:last_modified(ItemPath),
-    ItemProperties = [
-      {"path", ItemPath}
-    ],
+    ItemProperties = [{"path", ItemPath}],
 
     case filelib:is_dir(ItemPath) of
       true ->
-        erlang:display("Dir"),
-        erlang:display(ItemPath),
-        UpdatedTree = gb_trees:enter(ItemPath, ItemProperties, Tree),
-        build_tree(ItemPath, UpdatedTree);
+        UpdatedTree = gb_trees:enter(ItemPath, ItemProperties, AccumulatedTree),
+        build(ItemPath, UpdatedTree);
       _ ->
-        erlang:display("File"),
-        erlang:display(ItemPath),
-        gb_trees:enter(ItemPath, [{"modified_at", LastModifiedAt} | ItemProperties], Tree)
+        FileProps = [{"modified_at", LastModifiedAt} | ItemProperties],
+        gb_trees:enter(ItemPath, FileProps, AccumulatedTree)
     end
   end,
-  lists:map(ItemMapper, Items).
+  lists:foldl(ItemFolder, Tree, Items).
