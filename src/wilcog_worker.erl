a9bc042c0c0deb7fbda177c7612f6103169b4b63
Akash Manohar J <akash@akash.im>
2014-03-03 13:33:56 +0530

    Add missing callbacks

--- src/wilcog_worker.erl
+++ src/wilcog_worker.erl
@@ -3,7 +3,8 @@
 
 -export([start_link/0]).
 -export([rebuild/1, watch/2]).
--export([init/1, handle_call/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
 
 
 rebuild(Group) ->
@@ -21,6 +22,10 @@ init(_Args) ->
   {ok, dict:new()}.
 
 
+handle_call(stop, _From, State) ->
+  {stop, normal, stopped, State};
+
+
 handle_call({watch, Group, AssetPath, Options}, _From, State) ->
   case dict:find(Group, State) of
     error ->
@@ -31,7 +36,6 @@ handle_call({watch, Group, AssetPath, Options}, _From, State) ->
       {reply, group_already_exists, State}
   end;
 
-
 handle_call({rebuild, Group}, _From, State) ->
   case dict:find(Group, State) of
     {ok, {AssetPath, Tree, Options}} ->
@@ -41,3 +45,18 @@ handle_call({rebuild, Group}, _From, State) ->
     error ->
       {reply, no_group_found, State}
   end.
+
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
