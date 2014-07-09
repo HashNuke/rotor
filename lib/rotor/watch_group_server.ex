585be6644e96b7eb592041912e552ec05e0abbaf
Akash Manohar J <akash@akash.im>
2014-07-09 13:02:07 +0530

    Cleanup watch group server

--- lib/rotor/watch_group_server.ex
+++ lib/rotor/watch_group_server.ex
@@ -5,45 +5,46 @@ defmodule Rotor.WatchGroupServer do
   end
 
 
-  def add_group(name, paths, file_index, rotor_function, options \\ %{}) do
-    {:ok, default_limit} = Rotor.ConfigServer.get(:parallel_limit)
-    limit = Map.get(options, :parallel_limit, default_limit)
-
-    Agent.get_and_update __MODULE__, fn(groups)->
-      group = %{
-        :paths => paths,
-        :file_index => file_index,
-        :rotor_function => rotor_function,
-        :parallel_limit => limit
-      }
-
-      updated_groups = Map.put groups, name, group
-      :ok = Rotor.FileWatcher.add_group(name)
-      {:ok, updated_groups}
-    end
-  end
-
-
-  def get_group(name) do
+  def group(name) do
     Agent.get __MODULE__, fn(state)->
       {:ok, state[name]}
     end
   end
 
 
-  def update_file_index(name, new_index) do
-    Agent.get_and_update __MODULE__,  fn(groups)->
-      group = %{groups[name] | :file_index => new_index}
-      updated_groups = Map.put groups, name, group
+  def add(name, paths, rotor_function, options \\ %{}) do
+    Agent.get_and_update __MODULE__, fn(groups)->
+      group = %{
+        paths: paths,
+        rotor_function: rotor_function,
+        options: options
+      }
+
+      updated_groups = put_in groups[name], group
+      :ok = Rotor.FileWatcher.add_group(name)
       {:ok, updated_groups}
     end
   end
 
 
-  def remove_group(name) do
+  def remove(name) do
     Agent.get_and_update __MODULE__,  fn(groups)->
       updated_groups = Map.delete groups, name
       {:ok, updated_groups}
+    end
+  end
+
+
+  def trigger(name, changed_files, all_files) do
+    Agent.get __MODULE__, fn(state)->
+      group = get_in state, [name]
+
+      try do
+        apply group.rotor_function, [changed_files, all_files]
+      rescue
+        _ ->
+          IO.puts "Some problem running rotor function for group: #{name}"
+      end
     end
   end
 
